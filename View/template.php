e2e42d2e84350ebffa91e228d49850daa4ff7256
miroslavpenkov <miroslavpenkov12@gmail.com>
2023-10-22 22:29:35 +0200

    h

--- View/template.php
+++ View/template.php
@@ -25,7 +25,6 @@
 
     </header>
 
-
             <main class="container mt-4"> <!-- Ajout de l'espace -->
                 <div class="row">
                     <div class="col-md-8">
@@ -34,7 +33,8 @@
                     </div>
                 </div>
             </main>
-            <!-- Charger vos fichiers JavaScript ici -->
+
+            <!-- Chargement des fichiers JavaScript ici -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
