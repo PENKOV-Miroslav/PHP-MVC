e37510473729520b38db26097dca0a7a1b2fec19
miroslavpenkov <miroslavpenkov12@gmail.com>
2023-10-23 14:14:31 +0200

    estetique

--- View/PageEspaceAdmin.php
+++ View/PageEspaceAdmin.php
@@ -7,8 +7,8 @@ if (isset($_SESSION['user_id']) && $_SESSION['id_role'] === 1) {
     ?>
     <p>Vous êtes connecté en tant qu'administrateur.</p>
     <button class="btn btn-primary" id="btnAfficherInscription" onclick="afficherPageInscription()">Inscrire un nouveau compte</button>
-    <div id="resultatInscription"></div>
     <a class="btn btn-danger" href="?action=deconnexion">Déconnexion</a>
+    <div id="resultatInscription"></div>
     <?php
 } else {
     // L'utilisateur n'est pas authentifié en tant qu'administrateur, redirigez-le vers la page d'authentification
