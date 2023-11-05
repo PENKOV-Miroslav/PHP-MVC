bb06133c394bd7e1939258094405624e81aa639a
miroslavpenkov <miroslavpenkov12@gmail.com>
2023-11-05 22:15:11 +0100

    LAST UPDATE

--- View/pointageRfid.php
+++ View/pointageRfid.php
@@ -3,6 +3,16 @@
     <form method="POST" action="?action=rfid">
         <div class="mb-3 d-flex align-items-center">
             <input type="text" id="rfidInput" name="rfid" placeholder="Entrez le code RFID" class="form-control" required>
+            <select class="form-control" name="option" id="options" required>
+                <option value="" disabled selected>--- Epreuve bol'air ---</option>
+                <option value="1">Course à pieds</option>
+                <option value="2">Canoe</option>
+                <option value="3">VTT</option>
+                <option value="" disabled>--- Epreuve mini bol'air ---</option>
+                <option value="4">Course à pieds</option>
+                <option value="5">Canoe</option>
+                <option value="6">VTT</option>
+            </select>
             <button id="sendRFID" onclick="getRFID()" class="btn btn-primary">Envoyer</button>
         </div>
         <div id="result"></div>
